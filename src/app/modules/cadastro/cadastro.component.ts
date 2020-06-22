da3aab800617b5c548c1415497b4f255beb727e6
Evelise Vazquez <evelisevazquez@gmail.com>
2020-06-22 18:19:22 -0300

    feat: requisicoes async

--- src/app/modules/cadastro/cadastro.component.ts
+++ src/app/modules/cadastro/cadastro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient, HttpResponseBase } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators'
 
 @Component({
   selector: 'cmail-cadastro',
@@ -8,25 +10,52 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   ]
 })
 export class CadastroComponent implements OnInit {
+  
+  constructor(
+    private httpClient: HttpClient
+  ) { }
 
-  isOkay = true;
+  validaImagem(campoDoFormulario: FormControl){
+    console.log(campoDoFormulario)
+    return this.httpClient
+      .head(campoDoFormulario.value, {
+        observe: 'response'
+      })
+      .pipe(
+        map((response: HttpResponseBase) => {
+          console.log(response)
+          return response.ok ? null : { urlInvalida: true}
+        }),
+      catchError((error) => {
+        return [{urlInvalida: true}]
+      })
+      )
+  }
 
   formCadastro = new FormGroup({
-    nome: new FormControl('', Validators.required),
-    username: new FormControl('', Validators.required),
-    senha: new FormControl('', [Validators.required, Validators.minLength(2)]),
-    avatar: new FormControl('', Validators.required),
+    nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    username: new FormControl('', [Validators.required]),
+    senha: new FormControl('', [Validators.required]),
+    telefone: new FormControl('', [Validators.required, Validators.pattern('[0-9]{4}-?[0-9]{4}[0-9]?')]),
+    avatar: new FormControl('', [], this.validaImagem.bind(this)),
   })
+  
+  ngOnInit(): void {}
 
-  constructor() { }
 
-  ngOnInit(): void {
+  validarTodosOsCamposDoFormulario(form: FormGroup){
+    Object.keys(form.controls).forEach(field => {
+      const control = form.get(field);
+      control.markAsTouched({ onlySelf: true})
+    })
   }
 
   handleCadastrarUsuario() {
     console.log(this.formCadastro)
-    if(this.formCadastro.invalid) {
-      console.log('invalido')
+    if(this.formCadastro.valid) {
+      this.formCadastro.reset();
+    } else {
+      this.validarTodosOsCamposDoFormulario(this.formCadastro);
     }
   }
 
